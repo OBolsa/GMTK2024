3e010159e4204f5f5b14979c39fafecd629904c9
Vitor Moreira <vitor-hug@hotmail.com>
2024-08-16 15:38:18 -0300

    Initial Setup Prototype 01

---
+++ Assets/Systems/Player/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    private Rigidbody2D rb;
+
+    private Vector2 desiredDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        desiredDirection = PlayerInputManager.Inputs.World.Movement.ReadValue<Vector2>();
+
+        if (desiredDirection.magnitude > 0.1f)
+        {
+            rb.velocity = desiredDirection.normalized * movementSpeed * Time.deltaTime;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}

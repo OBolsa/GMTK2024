9ef2cc7ce435332b01352c6e05afdd68b776e08d
Michael Barbosa Santos <michaerl.barbosa.santos@hotmail.com>
2024-08-16 22:06:03 -0300

    UI_INVENTARIO, placeholder e UI

---
+++ Assets/UI_Inventario.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Inventario : MonoBehaviour
+{
+    // Start is called before the first frame update
+   
+    //public int totalQuantityItems;
+        
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+}

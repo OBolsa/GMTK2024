3591e04def2f9928b3c06a49b6b6db88ede1665d
João Paulo Zaneti Cruz <jphdm20@hotmail.com>
2024-08-17 18:01:27 -0300

    SmokeVFX
    SmoveVFX And AudioManager Prefab Fix

--- Assets/Systems/Gameplay/Interaction/DestroyableObject.cs
+++ Assets/Systems/Gameplay/Interaction/DestroyableObject.cs
@@ -11,11 +11,16 @@ public class DestroyableObject : MonoBehaviour, IInteractable
     private float currentHealth;
 
     public UnityEvent OnDestroy;
+    private ParticleHander myParticles;
+
 
     private void Start()
     {
         currentHealth = maxHealth;
         lifeGauge = transform.Find("WorldCanvas").Find("LifeGauge").GetComponent<Image>();
+        myParticles = GetComponent<ParticleHander>();
+
+
     }
 
     public void SetupDestroyable(int currentHealth, int maxHealth)
@@ -36,6 +41,8 @@ public class DestroyableObject : MonoBehaviour, IInteractable
             currentHealth -= 1;
             lifeGauge.fillAmount = (float)(currentHealth / maxHealth);
             Debug.Log("Interacting, currentLife:" + currentHealth);
+            if (myParticles != null) myParticles.PlayVFX();
+            AudioManager.instance.PlaySfx(SFXs.SMASH_BUMP);
 
             if (currentHealth <= 0) Die();
         }

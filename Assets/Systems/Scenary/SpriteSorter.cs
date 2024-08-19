6417abfa0061ac84564c5e741a5db5c71c639970
Michael Barbosa Santos <michaerl.barbosa.santos@hotmail.com>
2024-08-19 15:50:13 -0300

    Level Spawner 03, Level Spawner 02,Level , alterando rule tile Limite do mapa , no class SpriteSorte add  void GeneratePosiçãoRandomValue()

--- Assets/Systems/Scenary/SpriteSorter.cs
+++ Assets/Systems/Scenary/SpriteSorter.cs
@@ -5,12 +5,24 @@ public class SpriteSorter : MonoBehaviour
 {
     public SpriteRenderer sr;
     public List<Sprite> sprites;
+    public float valorDesvio;
+    public bool gerarDesvio = false;
 
     void Start()
     {
         int random = Random.Range(0, sprites.Count);
         sr.sprite = sprites[random];
+        if(gerarDesvio)GeneratePosiçãoRandomValue(valorDesvio);
     }
+    public void GeneratePosiçãoRandomValue(float random)
+    {
+        
+        float posicaoX = Random.Range(-random, random);
+        float posicaoY = Random.Range(-random, random);
+        Vector3 addPossicao = new Vector3(posicaoX,posicaoY,0f); 
+        this.transform.position += addPossicao;
 
+        Debug.Log("addPossicao" + addPossicao);
+    }
 
 }
